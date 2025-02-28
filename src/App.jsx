00ac85cff7cedaf999ffb71526f78afcf7e857aa
spleereak <spleereak@gmail.com>
2025-03-01 00:20:04 +0300

    Delete App.css; Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='text-green-300 bg-gray-800 p-100'>
-      <p className='text-9xl text-center'>PROOOOOD</p>
+      <p className='text-9xl text-center'>App</p>
     </div>
   )
 }
