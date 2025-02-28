51bb6f0f8e5714da22b43d1b103dc6d146f8d724
Andrey Danilov <galagtik10@gmail.com>
2025-03-01 00:31:05 +0300

    feat: add BookingModel and BookingStatus enum for booking management

---
+++ src/main/java/prod/last/mainbackend/models/BookingModel.java
@@ -0,0 +1,60 @@
+package prod.last.mainbackend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "booking")
+public class BookingModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private UUID userId;
+
+    @NotBlank
+    private UUID placeId;
+
+    @NotBlank
+    private LocalDateTime startAt;
+
+    @NotBlank
+    private LocalDateTime endAt;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    @NotBlank
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    private LocalDateTime updatedAt;
+
+    public BookingModel() {}
+
+    public BookingModel(UUID userId, UUID placeId, LocalDateTime startAt, LocalDateTime endAt, BookingStatus status) {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.userId = userId;
+        this.placeId = placeId;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.status = status;
+    }
+
+    public void updateStatus(BookingStatus status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
