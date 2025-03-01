a40ecec97840eaf7b19e1396eb8419b59782a56d
spleereak <spleereak@gmail.com>
2025-03-01 03:46:03 +0300

    Update Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,11 @@
-function Auth() {}
+import { AuthForm } from "../components/AuthForm";
+
+function Auth() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <AuthForm />
+    </div>
+  )
+}
 
 export default Auth;
