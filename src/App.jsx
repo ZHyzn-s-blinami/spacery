c57ff8f2fa008a4e659ccfcfecfdbb2c333f9c4a
spleereak <spleereak@gmail.com>
2025-03-01 03:41:52 +0300

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import UserLayout from "./layouts/UserLayout";
 import AdminLayout from "./layouts/AdminLayout";
 import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
         <Route path='/auth' element={<Auth />} />
-      </Route>
-      <Route path='/admin' element={<AdminLayout />}>
-
       </Route>
     </Routes>
   )
