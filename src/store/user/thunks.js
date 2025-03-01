5eaed2527e741d62e6a468c8513292b5357d2a15
spleereak <spleereak@gmail.com>
2025-03-01 11:39:24 +0300

    add asyncThunks

---
+++ src/store/user/thunks.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { authService } from "../../services/authService";
+import { addUser } from "./slice";
+
+export const registerUser = createAsyncThunk(
+  'users/register',
+  async (userData, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await authService.register(userData);
+
+      dispatch(addUser(userData));
+
+      return response.user;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || { message: error.message });
+    }
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  'users/login',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await authService.login(credentials);
+      return response.user;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || { message: error.message });
+    }
+  }
+)
+
+export const logoutUser = createAsyncThunk(
+  'users/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      authService.logout();
+      return null;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+)
