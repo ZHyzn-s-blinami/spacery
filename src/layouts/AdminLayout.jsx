9831fb4df3c9edb29842ccf17773fd800c74582f
spleereak <spleereak@gmail.com>
2025-03-02 15:31:07 +0300

    fix auth checking №2

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 function AdminLayout() {
+  const userToken = localStorage.getItem('userToken');
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!userToken) {
+      navigate('/auth');
+    } else {
+      navigate('/admin');
+    }
+  }, [userToken]);
+
   return (
     <>
       <Outlet />
     </>
-  )
+  );
 }
 
 export default AdminLayout;
